00acd7ddf27fcefaed5f80dc5db1cd8db9bd6c23
Muhammed vasil <vasilnk7@gmail.com>
2024-09-03 14:14:12 +0530

    last error in app bar solved

--- lib/costom_widgets/appbar.dart
+++ lib/costom_widgets/appbar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:play_tune/utils/color.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
